ea931bc06951d2060afe3a470d9abc7704b1c285
James Kyburz <james.kyburz@gmail.com>
2020-12-06 14:35:54 +0000

    add aws credentials to cli.sh

--- cli.sh
+++ cli.sh
@@ -6,5 +6,9 @@ docker run \
   --rm \
   -ti \
   -v "${PWD:?}":/work \
+  -e "AWS_ACCESS_KEY_ID" \
+  -e "AWS_SECRET_ACCESS_KEY" \
+  -e "AWS_SESSION_TOKEN" \
+  -w /work \
   -v /var/run/docker.sock:/var/run/docker.sock \
   jameskyburz/ops-kitchen bash
